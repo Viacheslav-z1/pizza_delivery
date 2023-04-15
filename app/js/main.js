66b826f65db8654a6f019fc32580cee72616a37c
slavik <slavik.zaliotnyi@gmail.com>
2023-04-15 16:16:02 +0300

    added slider tovars

--- app/js/main.js
+++ app/js/main.js
@@ -19,4 +19,43 @@ $(function () {
   function openMenu() {
     menu.classList.add("open");
   }
+
+
+
+
+
+
+
+
+  /*slick */
+
+    $(".tovars__list").slick({
+      slidesToShow: 5,
+      prevArrow:
+        '<button type="button" class="slick-prev"><img src="../images/arrow-left.svg" alt=""></button>',
+      nextArrow:
+        '<button type="button" class="slick-next"><img src="../images/arrow-right.svg" alt=""></button>',
+      autoplay: true,
+      autoplaySpeed: 4000,
+      responsive: [
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 550,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
 });
