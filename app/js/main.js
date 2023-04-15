c791c829e4d7236f9c382810e7a58d323ee9d747
slavik <slavik.zaliotnyi@gmail.com>
2023-04-15 15:17:12 +0300

    added header  and about section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function () {
+  const openBtn = document.querySelector(".menu__open");
+  const closeBtn = document.querySelector(".menu__close");
+  const menu = document.querySelector(".menu__list");
 
-})
+  openBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    menu.classList.add("open");
+  });
+
+  closeBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    closeMenu();
+  });
+
+  function closeMenu() {
+    menu.classList.remove("open");
+  }
+  function openMenu() {
+    menu.classList.add("open");
+  }
+});
